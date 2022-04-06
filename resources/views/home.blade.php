a6a2b7c5f481aba6aa8aed57313d0cd8678de9f9
Ozhin <ozhyn5@gmail.com>
2022-04-06 20:53:33 +0300

    installation

---
+++ resources/views/home.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>WELCOME</h1> 
+    <h4>this is the links:</h4>
+    <a href="/profile">Link 1</a><br>
+    <a href="/profile">Link 2</a>
+</body>
+</html>
---
+++ resources/views/profile.blade.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <style>
+        .div1{
+            width:300px;
+            height:200px;
+            border:3px solid red;
+        }
+        .div2{
+            width:300px;
+        }
+    </style>
+</head>
+<body>
+    <div class="div1"><img src="\images\Desert.jpg" height="200px" width="300px" ></div>
+    <div class="div2"><fieldset><legend> bio </legend>
+        <p>hello, my name is ozhin, and this is my assingment.</p>
+
+</fieldset></div>
+</body>
+</html>
